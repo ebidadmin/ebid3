999b2b4c083e9030461fb7ad4174095290f3fd4d
Chris Marquez <cymarquez@mac.com>
2010-11-30 18:14:51 +0800

    Installed jquery adapter.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def nav_link(text, page, clas=nil, id = nil) 
+    content_tag :li, (link_to text, page, :id => current?(page)), :class=> clas
+  end 
+
+  def current?(page_name)
+    "current_page" if current_page? page_name
+  end
+
 end
