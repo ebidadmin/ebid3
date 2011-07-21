89c3eba82cafc5162642e8b0ce1958ee741d9c71
Chris Marquez <cymarquez@mac.com>
2011-07-21 16:02:13 +0800

    Problem was with Paperclip 2.3.15!

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,21 +9,7 @@ set :scm, :git
 # set :scm_username, 'chrism'
 # set :scm_password, 'Beanstalk21' 
 
-# set :domain, "74.207.254.124"
-# set :user, "root" 
-# set :use_sudo, false
-# set :deploy_to, "/srv/www/ebid.com.ph"
-# ssh_options[:forward_agent] = true
-# set :branch, "master"
-# set :git_shallow_clone, 1
-# set :git_enable_submodules, 1
-# set :rails_env, "production"
-# 
-# role :web, "74.207.254.124"                          # Your HTTP server, Apache/etc
-# role :app, "74.207.254.124"                          # This may be the same as your `Web` server
-# role :db,  "74.207.254.124", :primary => true        # This is where Rails migrations will run
-
-set :domain, "ebidlabs.com"
+set :domain, "74.207.254.124"
 set :user, "root" 
 set :use_sudo, false
 set :deploy_to, "/srv/www/ebid.com.ph"
@@ -31,12 +17,26 @@ ssh_options[:forward_agent] = true
 set :branch, "master"
 set :git_shallow_clone, 1
 set :git_enable_submodules, 1
-# set :rails_env, "development"
 set :rails_env, "production"
-                                                    
-role :web, "ebidlabs.com"                         
-role :app, "ebidlabs.com"                         
-role :db,  "ebidlabs.com", :primary => true       
+
+role :web, "74.207.254.124"                          # Your HTTP server, Apache/etc
+role :app, "74.207.254.124"                          # This may be the same as your `Web` server
+role :db,  "74.207.254.124", :primary => true        # This is where Rails migrations will run
+
+# set :domain, "ebidlabs.com"
+# set :user, "root" 
+# set :use_sudo, false
+# set :deploy_to, "/srv/www/ebid.com.ph"
+# ssh_options[:forward_agent] = true
+# set :branch, "master"
+# set :git_shallow_clone, 1
+# set :git_enable_submodules, 1
+# # set :rails_env, "development"
+# set :rails_env, "production"
+#                                                     
+# role :web, "ebidlabs.com"                         
+# role :app, "ebidlabs.com"                         
+# role :db,  "ebidlabs.com", :primary => true       
                                                     
 
 namespace :deploy do
